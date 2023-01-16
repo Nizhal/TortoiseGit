72a23508fdab30f0305a5bc2d9b61e28795b2ef7
தமிழ்நேரம் <anishprabu.t@gmail.com>
2023-01-17 03:13:31 +0530

    தமிழ் மொழி
    Signed-off-by: தமிழ்நேரம் <anishprabu.t@gmail.com>

---
+++ Languages/தமிழ்/Install.bat
@@ -0,0 +1,7 @@
+.\ResText.exe apply TortoiseProcLang.dll TortoiseProc1172.dll Tortoise_Ta.po
+.\ResText.exe apply TortoiseMergeLang.dll TortoiseMerge1172.dll Tortoise_Ta.po
+.\ResText.exe apply TortoiseIDiffLang.dll TortoiseIDiff1172.dll Tortoise_Ta.po
+.\ResText.exe apply TortoiseGitUDiffLang.dll TortoiseGitUDiff1172.dll Tortoise_Ta.po
+.\ResText.exe apply TortoiseGitBlameLang.dll TortoiseGitBlame1172.dll Tortoise_Ta.po
+pause
+
